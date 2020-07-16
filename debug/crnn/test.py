6de11db914461a9d1127898f04a153d77c21ac06
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2020-07-16 23:35:07 +0900

    crnn trainable

--- debug/crnn/test.py
+++ debug/crnn/test.py
@@ -4,15 +4,15 @@ from dl.data.txtrecog import datasets
 from dl.models import CRNN
 
 if __name__ == '__main__':
-    model = model = CRNN(class_labels=datasets.ALPHANUMERIC_WITH_BLANK_LABELS, input_shape=(32, 100, 1)).cuda()
+    model = CRNN(class_labels=datasets.ALPHANUMERIC_WITH_BLANK_LABELS, input_shape=(32, 100, 1)).cuda()
     model.eval()
-    model.load_weights('../../weights/crnn-synthtext/i-3000000_checkpoints20200710.pth')
+    model.load_weights('../../weights/crnn-synthtext/e-0000030_checkpoints20200712.pth')
     print(model)
 
     image = cv2.cvtColor(cv2.imread('../../scripts/crnn/assets/test.jpg'), cv2.COLOR_BGR2GRAY)
-    image = cv2.cvtColor(cv2.imread('../../scripts/crnn/assets/demo.png'), cv2.COLOR_BGR2GRAY)
-    #cv2.imshow('test image', image)
-    #cv2.waitKey()
+    #image = cv2.cvtColor(cv2.imread('../../scripts/crnn/assets/demo.png'), cv2.COLOR_BGR2GRAY)
+    cv2.imshow('test image', cv2.resize(image, (100, 32)))
+    cv2.waitKey()
     _, raws, outs = model.infer(image, toNorm=True)
     for raw, out in zip(raws, outs):
         print('{} -> {}'.format(raw, out))
